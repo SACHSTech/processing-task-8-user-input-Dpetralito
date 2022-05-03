77c4ad1a24fe091c75446c9837bfdbf9ff3ffa3c
danielpetralito <99832159+Dpetralito@users.noreply.github.com>
2022-05-03 03:57:09 +0000

    done

--- Sketch.java
+++ Sketch.java
@@ -1,8 +1,10 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
+
+  String message = "";
+  float circleX = 100;
+  float circleY = 100;
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -16,21 +18,63 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255);
+    textSize(30);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    text(message, 25, 200);
+    
+	if(keyPressed){
+    if(key == 'r'){
+      background(255, 0, 0);
+    }else if(key == 'b'){
+      background(0, 4, 255);
+    }else if(key == 'g'){
+      background(0, 255, 0);
+    }
   }
+  
+  if (mousePressed) {
+    
+  ellipse(mouseX, mouseY, 25, 25);
+  }
+    
+  }
+
+  public void keyPressed() {
+    if (keyPressed) {
+    if (keyCode == UP) {
+      circleY--;
+    } 
+    else if (keyCode == DOWN) {
+      circleY++;
+    } 
+    else if(keyCode == LEFT){
+      circleX--;
+    }
+    else if(keyCode == RIGHT){
+     circleX++; 
+    }
+  } 
+
+  ellipse(circleX, circleY, 50, 50);
+}
+  
+  public void mouseDragged(){
+
+    //make a sun
+    fill(252, 227, 3);
+    ellipse((float) (width/1.333), (float) (height*.175), (float) (width/5), (float) (height/4));
+  }
+
+  public void keyTyped() {
+  message += key;
+}
   
   // define other methods down here.
 }
